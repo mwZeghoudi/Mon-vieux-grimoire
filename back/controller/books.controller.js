236dbf167021e120753989c6ecda516d9068241f
iLnsiC <besswalid@gmail.com>
2023-05-26 16:53:02 +0200

    [ADD] Book controller

---
+++ back/controller/books.controller.js
@@ -0,0 +1,18 @@
+const db = require("./config/db");
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+const Books = db.books;
+
+exports.listAction = async (req, res) => {};
+
+exports.getAction = async (req, res) => {};
+
+exports.bestListAction = async (req, res) => {};
+
+exports.addAction = async (req, res) => {};
+
+exports.addRatingAction = async (req, res) => {};
+
+exports.editAction = async (req, res) => {};
+
+exports.editAction = async (req, res) => {};
