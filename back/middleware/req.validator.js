e45366fadcc5315cc2d950f732cc35f45856be5f
iLnsiC <besswalid@gmail.com>
2023-06-15 06:09:37 +0200

    [WIP] text validqtion input

--- back/middleware/req.validator.js
+++ back/middleware/req.validator.js
@@ -4,28 +4,28 @@ module.exports = (req, res, next) => {
   const emailRegex = /^\S+@\S+\.[a-zA-Z]{3}$/;
   const imageFormatRegex = /\.(jpg|jpeg|png|webp)$/i;
   const yearRegex = /^\d{4}$/;
-  const numericRegex = /^\d+$/;
 
   function deleteImage() {
-    console.log(req.files);
-    console.log(req.file.filename);
-    console.log(req.file.path);
     fs.unlinkSync(req.file.path);
   }
+
   // Adapter la requete si un fichier y est attacher
   let reqObject = req.body;
   if (reqObject.book) {
     reqObject = JSON.parse(req.body.book);
   }
 
+  // Validation du contenu des input text
+  // reqObject.title.replace(/[^a-zA-Z0-9\s]/g, "");
+  // reqObject.author.replace(/[^a-zA-Z0-9\s]/g, "");
+  // reqObject.genre.replace(/[^a-zA-Z0-9\s]/g, "");
+
   // Validation de l'email si présent
   if (reqObject.email && !emailRegex.test(reqObject.email)) {
     req.body.book ? deleteImage() : null;
     return res.status(400).json({ error: "Format d'email invalide." });
   }
-  // console.log(reqObject.year);
-  // console.log(yearRegex.test(reqObject.year));
-  // console.log(reqObject.year.length !== 4);
+
   // Validation de l'année si présente
   if (
     reqObject.year &&
