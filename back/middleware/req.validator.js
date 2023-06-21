2f9340e71f2b95e3b98e2a8b07d9e8600f732f73
Mohammed-Walid Zeghoudi <mwzeghoudi@santeformapro.com>
2023-06-21 09:57:48 +0200

    [PATCH] Regex

--- back/middleware/req.validator.js
+++ back/middleware/req.validator.js
@@ -1,43 +1,32 @@
 const fs = require("fs");
+const emailRegex = /^\S+@\S+\.[a-zA-Z]{2,3}$/;
+const passwordRegex = /^\S+$/;
+const imageFormatRegex = /\.(jpg|jpeg|png|webp)$/i;
+const yearRegex = /^\d{4}$/;
 
 module.exports = (req, res, next) => {
-  const emailRegex = /^\S+@\S+\.[a-zA-Z]{2,3}$/;
-  const passwordRegex = /^\S+$/;
-  const imageFormatRegex = /\.(jpg|jpeg|png|webp)$/i;
-  const yearRegex = /^\d{4}$/;
 
-  function deleteImage() {
-    fs.unlinkSync(req.file.path);
-  }
-
-  // Adapter la requete si un fichier y est attacher
   let reqObject = req.body;
   if (reqObject.book) {
     reqObject = JSON.parse(req.body.book);
   }
-
-  // Validation du contenu des input text
-  if (reqObject.title || reqObject.author || reqObject.genre) {
-    const filteredTitle = reqObject.title.replace(/[^a-zA-Z0-9\s]/g, "");
-    const filteredAuthor = reqObject.author.replace(/[^a-zA-Z0-9\s]/g, "");
-    const filteredGenre = reqObject.genre.replace(/[^a-zA-Z0-9\s]/g, "");
-
-    reqObject.title = filteredTitle;
-    reqObject.author = filteredAuthor;
-    reqObject.genre = filteredGenre;
+  function deleteImage() {
+    console.log(req.file.path);
+    fs.unlinkSync(req.file.path);
   }
 
-  // Validation de l'email si présent
+  reqObject.title ? reqObject.title = reqObject.title.replace(/[^a-zA-Z0-9\s]/g, "") : null;
+  reqObject.author ? reqObject.author = reqObject.author.replace(/[^a-zA-Z0-9\s]/g, "") : null;
+  reqObject.genre ? reqObject.genre = reqObject.genre.replace(/[^a-zA-Z0-9\s]/g, "") : null;
+
   if (reqObject.email && !emailRegex.test(reqObject.email)) {
-    req.body.book ? deleteImage() : null;
     return res.status(400).json({ error: "Format d'email invalide." });
   }
 
   if (reqObject.password && !passwordRegex.test(reqObject.password)) {
-    // Gérez l'erreur de mot de passe invalide (contenant des espaces)
     return res.status(400).json({ error: "Format du mot de passe invalide." });
   }
-  // Validation de l'année si présente
+
   if (
     reqObject.year &&
     !yearRegex.test(reqObject.year) &&
@@ -50,7 +39,6 @@ module.exports = (req, res, next) => {
     });
   }
 
-  // Validation du fichier image si présent
   if (req.files && req.files.image) {
     const imageFile = req.files.image;
     const fileExtension = imageFile.name.split(".").pop().toLowerCase();
